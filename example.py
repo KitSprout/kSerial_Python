b1d6812bbbd30af7d3775af9d81147f2283e6b82
Hom-Wang <wenhom.wang@gmail.com>
2021-08-09 22:25:26 +0800

    add plotter

--- example.py
+++ example.py
@@ -3,6 +3,10 @@ import glob
 import serial
 from kserial import kserial
 
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 
 def port_list():
     if sys.platform.startswith('win'):
@@ -54,8 +58,55 @@ def continuous(ks):
     ks.target_mode(0)
 
 
-# main process
+def plotupdate(i, ks, lines, lines_value, line_label, width, ys, line_num):
+    pkinfo, pkdata, pkcount, pkdt = ks.continuous()
+    if pkcount != 0:
+        for i in range(line_num):
+            for k in range(pkcount):
+                y_data = pkdata[k][i+5] / 8192.0 * 9.8
+                ys[i].append(y_data)
+            ys[i] = ys[i][-width:]
+            lines[i].set_ydata(ys[i])
+            lines_value[i].set_text(f'[{line_label[i]}] {y_data:9.5f} m/s^2')
+    return lines[0], lines[1], lines[2]
 
+
+def plotsingle(ks):
+    # kserial mode
+    ks.target_rate(100)
+    ks.target_mode(1)
+    # Parameters
+    width = 100
+    y_range = [-40, 40]
+    line_num = 3
+    line_label = ['X', 'Y', 'Z']
+    line_style = ['r', 'g', 'b']
+    # create figure
+    fig, ax = plt.subplots(figsize=(12, 6))
+    ax.set_xlim([0, width])
+    ax.set_ylim(y_range)
+    # create line
+    xs = list(range(0, width))
+    ys = []
+    lines = []
+    lines_value = []
+    for i in range(line_num):
+        ys.append([0]*width)
+        lines.append(ax.plot(xs, ys[i], line_style[i], label=line_label[i])[0])
+        lines_value.append(ax.text(0.82, 0.95-i*0.05, '', transform=ax.transAxes))
+    len(lines)
+    len(ys)
+    plt.title('title')
+    plt.xlabel('samples')
+    plt.ylabel('data')
+    ani = animation.FuncAnimation(fig, plotupdate, fargs=(ks, lines, lines_value, line_label, width, ys, line_num,),
+        interval=0.001)
+    # ani = animation.FuncAnimation(fig, plotupdate, fargs=(ks, lines, lines_value, line_label, width, ys, line_num,),
+    #     interval=0.001, blit=True)
+    plt.show()
+
+
+# main process
 ports = port_list()
 if len(ports) < 1:
     print('not available com port')
@@ -74,5 +125,6 @@ print(f'  >> device check: {deviceid:X}')
 
 i2cdevice(ks)
 continuous(ks)
+plotsingle(ks)
 
 ks.close()
